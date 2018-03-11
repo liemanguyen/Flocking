1a36e28683dda7ab579dfa9ceb6bfa440cf6f888
Liem Nguyen <liemnguyen974@gmail.com>
2018-03-10 22:09:38 -0800

    moving repos

---
+++ Assets/Scripts/flock.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flock : MonoBehaviour {
+
+    public float speed = 0.001f;
+    float rotationSpeed = 4.0f;
+    Vector3 averageHeading;
+    Vector3 averageSpeed;
+    float neighborDistance = 2.0f;
+
+    public float speedMult = 1;
+
+	// Use this for initialization
+	void Start () {
+        speed = Random.Range(0.5f, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(transform.position, Vector3.zero) >= globalFlock.tankSize)
+        {
+            Vector3 direction = Vector3.zero - this.transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(direction),
+                                                  rotationSpeed * Time.deltaTime);
+            speed = Random.Range(0.5f, 1) * speedMult;
+        }
+        else
+        {
+            if (Random.Range(0, 5) < 1)
+            {
+                Flocking();
+            }
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed * speedMult);
+	}
+
+    void Flocking()
+    {
+        GameObject[] gos = globalFlock.allAnimals;
+
+        Vector3 vcenter = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.1f;
+
+        Vector3 goalPos = globalFlock.goalPos;
+
+        float dist;
+
+        int groupSize = 0;
+        foreach (GameObject go in gos)
+        {
+            if (go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+                if (dist <= neighborDistance)
+                {
+                    vcenter += go.transform.position;
+                    groupSize++;
+                    if (dist < 1.0f)
+                    {
+                        vavoid += this.transform.position - go.transform.position;
+                    }
+
+                    flock anotherFlock = go.GetComponent<flock>();
+                    gSpeed += anotherFlock.speed;
+                }
+            }
+
+            if (groupSize > 0)
+            {
+                vcenter = vcenter / groupSize + (goalPos - this.transform.position);
+                speed = gSpeed / groupSize * speedMult;
+
+                Vector3 direction = vcenter + vavoid - transform.position;
+                if (direction != Vector3.zero)
+                {
+                    transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                          Quaternion.LookRotation(direction),
+                                                          rotationSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/globalFlock.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalFlock : MonoBehaviour {
+
+    public GameObject animalPrefab;
+    public GameObject goalPrefab;
+    public static int tankSize = 5;
+
+    private static int numAnimals = 10;
+    public static GameObject[] allAnimals = new GameObject[numAnimals];
+
+    public static Vector3 goalPos = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < numAnimals; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-tankSize, tankSize),
+                                      Random.Range(-tankSize, tankSize),
+                                      Random.Range(-tankSize, tankSize));
+            allAnimals[i] = (GameObject)Instantiate(animalPrefab, pos, Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range(0, 10000) < 50)
+        {
+            goalPos = new Vector3(Random.Range(-tankSize, tankSize),
+                                  Random.Range(-tankSize, tankSize),
+                                  Random.Range(-tankSize, tankSize));
+            goalPrefab.transform.position = goalPos;
+        }
+	}
+
+    public void AnimalSpeed(float speedMult)
+    {
+        Debug.Log(speedMult);
+        for (int i = 0; i < numAnimals; i++)
+        {
+            allAnimals[i].GetComponent<flock>().speedMult = speedMult;
+        }
+    }
+}
